22bdfd485bd3c68368c116eeb1e075648203c602
khanadat <dattekoda@gmail.com>
2025-05-15 17:49:33 +0900

    my_libft

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include "libft.h"
+#include <bsd/string.h>
 #include <stdio.h>
-
-int main(void)
-{
-    printf("hello\n");
+int	main(void) {
+	char dst[10] = "\0";
+	char dstorigin[10] = "\0";
+	char src[100] = "ohayo sayonara ohayo sayonara ohayo sayonara";
+	ft_strlcpy(NULL, src, 8);
+	// strlcpy(NULL, src, 8);
+	printf("ft: %s\n", dst);
+	printf("or: %s\n", dstorigin);
+	return 0;
 }
